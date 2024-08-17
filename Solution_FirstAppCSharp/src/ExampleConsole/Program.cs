1c76908926f23419d24beff7e18bedcc67b97225
Wesley Leite <leitewesleyleite@gmail.com>
2024-08-17 12:45:33 -0300

    Types of readings

---
+++ Solution_FirstAppCSharp/src/ExampleConsole/Program.cs
@@ -0,0 +1,26 @@
+﻿// Write and keep in the same line
+//Console.Write("Enter a letter for Write");
+
+// Write and move to the next line
+//Console.WriteLine("\nEnter a letter for WriteLine");
+
+int? opcao = null;
+
+while (opcao != 0)
+{
+    Console.Write("Enter a number:");
+
+    // Character code, read only one
+    //int value = Console.Read();
+    //Console.WriteLine(value);
+
+    // Shows key value and details
+    //ConsoleKeyInfo value = Console.ReadKey();   
+    //Console.WriteLine(value.KeyChar);
+
+    // Read the entire line
+    int value = int.Parse(Console.ReadLine()!);
+    Console.WriteLine(value);
+
+    opcao = value;
+}
